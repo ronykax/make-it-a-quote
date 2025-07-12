215e2006a14e1be37618d250ce671002dba9b8ac
Rony Kati <73883742+ronykax@users.noreply.github.com>
2025-07-12 13:19:28 +0530

    feat: add player moment & bullets

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello via Bun!");
+import "dotenv/config";
+import { App } from "@slack/bolt";
+
+const app = new App({
+    signingSecret: process.env.SLACK_SIGNING_SECRET,
+    token: process.env.SLACK_BOT_TOKEN,
+});
+
+(async () => {
+    await app.start(6969);
+    console.log("app is online!");
+})();
